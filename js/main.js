6233b401f60195e2c2ceb9dcd3f0abccc7fc8714
joa32660 <jos3266@naver.com>
2019-11-07 17:54:02 +0900

    거의 완성

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 
-$(".top-navi").children("li").mouseover(function(){
-	$(".navis").css({"display":"inline-block"});
-	$(".navis").stop().slideDown();
+$(".top-navi > li").mouseover(function(){
+	$(this).children(".navis").css({"visibility":"visible"});
+	$(this).children(".navis").stop().slideDown();
 });
-$(".top-navi").children("li").mouseleave(function(){
-	$(".navis").stop().slideUp(function(){
-		$(this).css({"display":"none"});
+$(".top-navi > li").mouseleave(function(){
+	event.stopPropagation();
+	$(this).children(".navis").stop().slideUp(function(){
+		$(this).css({"visibility":"hidden"});
 	});
 });
 
 var bannerSlide = new FxSlide({
 	slides : $(".banner-containers"),
+	cnt: 1,
+	speed: 1500,
+	delay: 2000
+});
+
+var letterSlide = new FxSlide({
+	slides : $(".letter-images"),
 	cnt: 1,
 	speed: 1500,
 	delay: 2000
